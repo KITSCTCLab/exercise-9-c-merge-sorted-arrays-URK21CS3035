6f3dda17d3a8bd316ace1dc818cedf85151a2c4d
ARUMUGAM MARVIN <109951950+URK21CS3035@users.noreply.github.com>
2022-10-09 10:02:55 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,10 +1,37 @@
 from typing import List
 
 def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-  # Write code here
-
-
-# Do not change the following code
+    '''
+    We have 4 attributes
+    nums1 : which is the list1 with sorted numbers
+    nums2 : which is the list2 withn sorted numbers
+    m: m is the number of elements we take from nums1
+    n: n is the number of elements we take from nums2
+    '''
+    smalla = nums1[0:m] # make a new list to keep elements till m and n respectivly
+    smallb = nums2[0:n]
+    # using merge sort, sort both the array and add the result in nums1
+    i = 0  
+    j = 0
+    k = 0
+    while(i<len(smalla) and j<len(smallb)):
+        if (smalla[i] <= smallb[j]):
+            nums1[k] = smalla[i]
+            i+=1
+        else:
+            nums1[k] = smallb[j]
+            j+=1
+        k+=1
+    while i<len(smalla):
+        nums1[k] = smalla[i]
+        i+=1
+        k+=1
+    while j<len(smallb):
+        nums1[k] = smallb[j]
+        j+=1
+        k+=1
+  
+# Do not change the following corde
 nums1 = []
 nums2 = []
 for item in input().split(', '):
